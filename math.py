f59ffb7bed1982b775b549c4e210782fd6702e1f
patilswapnalis <38377837+patilswapnalis@users.noreply.github.com>
2021-03-04 11:33:25 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+# Add implementation
+##New header
+def add(x,y):
+	return x+y
+
+# Add implementation
+# Subtract implementation
+def subtract(x,y):
+    pass
+
+# Add implementation
+# Multiply implementation
+def multiply(x,y):
+	pass
+
+# Add implementation
+# Divide implementation
+def divide(x,y):
+    pass
+
+def log(x):
+    pass
